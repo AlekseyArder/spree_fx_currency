9546a04a95eb3c5895909c02781404ed4ba39436
Aleksey <yanukovichan@gmail.com>
2017-09-20 03:40:20 +0300

    Update migration

--- db/migrate/20160908125149_create_spree_fx_rates.rb
+++ db/migrate/20160908125149_create_spree_fx_rates.rb
@@ -1,5 +1,5 @@
 class CreateSpreeFxRates < ActiveRecord::Migration[5.1]
-  def change
+  def up
     create_table :spree_fx_rates do |t|
       t.string :from_currency
       t.string :to_currency
@@ -7,5 +7,9 @@ class CreateSpreeFxRates < ActiveRecord::Migration[5.1]
 
       t.timestamps null: false
     end
+  end
+
+  def down
+    drop_table :spree_fx_rates
   end
 end
