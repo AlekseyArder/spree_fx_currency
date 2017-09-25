3d2e57617fd4a685a9db74f4b8251af323c2c0b6
Aleksey <yanukovichan@gmail.com>
2017-09-25 04:26:12 +0300

    Update override 2

--- app/overrides/add_link_to_admin_sidebar_menu.rb
+++ app/overrides/add_link_to_admin_sidebar_menu.rb
@@ -1,6 +1,6 @@
 Deface::Override.new(
   virtual_path: 'spree/admin/shared/sub_menu/_configuration',
   name: 'fx_rate_admin_sidebar_menu',
-  insert_bottom: '#sidebar-configuration nav',
+  insert_bottom: '[data-hook="admin_configurations_sidebar_menu"]',
   partial: 'spree/admin/shared/fx_rate_sidebar_menu'
 )
