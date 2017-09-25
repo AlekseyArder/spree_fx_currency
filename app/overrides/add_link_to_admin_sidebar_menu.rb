52cd657fed0f042fd8df870528cfcdbc02085efe
Aleksey <yanukovichan@gmail.com>
2017-09-25 04:22:27 +0300

    Change menu position

--- app/overrides/add_link_to_admin_sidebar_menu.rb
+++ app/overrides/add_link_to_admin_sidebar_menu.rb
@@ -1,6 +1,6 @@
 Deface::Override.new(
   virtual_path: 'spree/admin/shared/sub_menu/_configuration',
   name: 'fx_rate_admin_sidebar_menu',
-  insert_bottom: '#sidebar-configuration',
+  insert_bottom: '#sidebar-configuration nav',
   partial: 'spree/admin/shared/fx_rate_sidebar_menu'
 )
